2570e8407f8b5457ab73ea23b032476d6df282fe
ankitsh6929 <sharma.ankit6929@gmail.com>
2024-10-14 01:04:08 +0530

    Initial commit: set up the structure using create react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import Dashboard from './components/Dashboard';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 import './App.css';
+import 'react-date-range/dist/styles.css'; 
+import 'react-date-range/dist/theme/default.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <Header />
+      <main className="main-content">
+        <Dashboard />
+      </main>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
